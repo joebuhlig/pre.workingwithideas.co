0d59ab5291b092b5db3b01bd98b75de1867e31c9
Joe Buhlig <joe@joebuhlig.com>
2016-06-09 07:06:11 -0500

    Updated mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_MAILER_HOST'] }
+  config.action_mailer.default_url_options = { :host => "workingwithideas.co" }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
